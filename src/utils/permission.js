62159a7fc2962d955e11b62ff1a58aa8ff037924
chifung <13059327127@qq.com>
2019-11-11 09:21:21 +0800

    fix Notify

--- src/utils/permission.js
+++ src/utils/permission.js
@@ -1,48 +1,53 @@
-import router from '@/router'
-import store from '@/store'
-import { Notify } from 'vant'
-import { getToken } from '@/utils/auth' // get token from cookie
-import getPageTitle from '@/utils/get-page-title'
+import router from "@/router";
+import store from "@/store";
+import { Notify } from "vant";
+import { getToken } from "@/utils/auth"; // get token from cookie
+import getPageTitle from "@/utils/get-page-title";
 
-const whiteList = ['/login', '/register'] // 白名单列表
+const whiteList = ["/login", "/register"]; // 白名单列表
 
 router.beforeEach(async (to, from, next) => {
   // 设置页面标题
-  document.title = getPageTitle(to.meta.title)
+  document.title = getPageTitle(to.meta.title);
 
   // determine whether the user has logged in
-  const hasToken = getToken()
+  const hasToken = getToken();
 
   if (hasToken) {
-    if (to.path === '/login') {
+    if (to.path === "/login") {
       // 已经登录，跳转到首页
-      next({ path: '/' })
+      next({ path: "/" });
     } else {
       // 获取用户信息
-      const hasGetUserInfo = store.getters.userData && store.getters.userData.name
+      const hasGetUserInfo =
+        store.getters.userData && store.getters.userData.name;
       if (hasGetUserInfo) {
-        next()
+        next();
       } else {
-        try {
-          // get user info
-          await store.dispatch('user/getInfo')
-          next()
-        } catch (error) {
-          // 清除用户信息，退出登录，跳转登录页
-          store.commit('user/LOGOUT')
-          Notify.error(error || 'Has Error')
-          next(`/login?redirect=${to.path}`)
-        }
+        // get user info
+        store
+          .dispatch("user/getInfo")
+          .then(() => {
+            next();
+          })
+          .catch(err => {
+            // 清除用户信息，退出登录，跳转登录页
+            store.commit("user/LOGOUT");
+            Notify({
+              type: "danger",
+              message: err.toString() || "Has Error"
+            });
+          });
       }
     }
   } else {
     /* has no token */
     if (whiteList.indexOf(to.path) !== -1) {
       // 白名单中，无需验证
-      next()
+      next();
     } else {
       // other pages that do not have permission to access are redirected to the login page.
-      next(`/login?redirect=${to.path}`)
+      next(`/login?redirect=${to.path}`);
     }
   }
-})
+});
